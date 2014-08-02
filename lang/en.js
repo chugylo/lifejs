ae0eb5d2d25c6a9c4aa5be55777548488cd7558d
chugylo <none@none>
2014-08-03 02:37:15 +0300

    Add basic i18n.

---
+++ lang/en.js
@@ -0,0 +1,47 @@
+LifeGameLang.en = {
+    title: "The Life Game"
+  , header: "The Life Game"
+
+  , fitAlert: '"Fit to window" function will hide scrolls and auto-run the game. Press ESC to show the scrolls back.'
+  , hugeConfirm1: "You are going to create a HUGE board ("
+  , hugeConfirm2: " cells). Are you sure?"
+
+    // PANEL
+    // p - prefix for a panel
+    // pi - prefix for an info panel
+  , pInfo: "Info"
+  , piStatus: "Status: "
+  , piStatusStopped: "stopped"
+  , piStatusRunning: "running"
+  , piStatusPaused: "paused"
+  , piGeneration: "Generation: "
+  , piCellInfo: "Cell Info: "
+  , piCellInfoEmpty: "&lt;Hover the mouse above the board to see&gt;"
+  , piCellInfoAlive: "alive"
+  , piCellInfoDead: "dead"
+  , piCellInfoBorder: "&lt;Cells' border&gt;"
+  , piDelay: "Delay: "
+  , piMs: "ms"
+  , piFps: "fps"
+  , piBoardSize: "Board Size: "
+  , piCells: "cells"
+  , piBoardEngine: "Board Engine: "
+  , pStepDelay: "Step Delay: "
+  , pStepDelayMs: "&#8201;ms"
+  , pRun: "Run"
+  , pPause: "Pause"
+  , pNewGame: "New Game"
+  , pBoardSize: "Board Size: "
+  , pFit: " Fit to window size"
+  , pFilling: "Initial Filling: "
+  , pRandom25: "Random 25% alive"
+  , pAllDead: "All dead"
+  , pAllAlive: "All alive"
+  , pStart: "Start new game"
+  , pBoardEngine: "Board Engine"
+  , pCanvasEngine: " Canvas"
+  , pDOMEngine: " DOM"
+  , pTip: "Click left mouse to make the cell alive and click right to make it dead."
+
+  , rights: "All rights reserved."
+}
---
+++ lang/uk.js
@@ -0,0 +1,47 @@
+LifeGameLang.uk = {
+    title: "Гра «Життя»"
+  , header: "Гра «Життя»"
+
+  , fitAlert: "Смуги прокрутки будуть приховані і гра почнеться автоматично. Натисніть клавішу ESC, щоб показати смуги прокрутки знову."
+  , hugeConfirm1: "Ви намагаєтесь створити величезну дошку ("
+  , hugeConfirm2: " чарунок). Підтвердити?"
+
+    // PANEL
+    // p - prefix for a panel
+    // pi - prefix for an info panel
+  , pInfo: "Інфо"
+  , piStatus: "Статус: "
+  , piStatusStopped: "зупинено"
+  , piStatusRunning: "грає"
+  , piStatusPaused: "на паузі"
+  , piGeneration: "Покоління: "
+  , piCellInfo: "Чарунка: "
+  , piCellInfoEmpty: "&lt;Наведіть мишку на дошку, щоб побачити&gt;"
+  , piCellInfoAlive: "жива"
+  , piCellInfoDead: "мертва"
+  , piCellInfoBorder: "&lt;Межа чарунки&gt;"
+  , piDelay: "Затримка: "
+  , piMs: "мс"
+  , piFps: "кадрів/с"
+  , piBoardSize: "Розмір дошки: "
+  , piCells: "чарунок"
+  , piBoardEngine: "Двигун гри: "
+  , pStepDelay: "Затримка кроку: "
+  , pStepDelayMs: "&#8201;мс"
+  , pRun: "Грати"
+  , pPause: "Пауза"
+  , pNewGame: "Нова гра"
+  , pBoardSize: "Розмір дошки: "
+  , pFit: " Підлаштувати під розмір вікна"
+  , pFilling: "Початковий вміст: "
+  , pRandom25: "25% випадково живих"
+  , pAllDead: "Всі мертві"
+  , pAllAlive: "Всі живі"
+  , pStart: "Почати нову гру"
+  , pBoardEngine: "Двигун гри"
+  , pCanvasEngine: " Canvas"
+  , pDOMEngine: " DOM"
+  , pTip: "Клікайте лівою кнопкою миші, щоб оживити чарунку або правою, щоб убити."
+
+  , rights: "Всі права збережено."
+}
