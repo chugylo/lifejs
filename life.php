cf28808f943d1c8e2f2a99e46d7ffd93bcd1bcb4
chugylo <none@none>
2014-08-06 21:30:21 +0300

    Add viewport and X-UA-Compatible meta-tags.

--- life.php
+++ life.php
@@ -6,6 +6,8 @@
 <html lang="<?= $lang; ?>">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <link rel="stylesheet" type="text/css" href="life.css">
   <script>var LifeGameLang = {};</script>
   <script src="lang/en.js"></script>
